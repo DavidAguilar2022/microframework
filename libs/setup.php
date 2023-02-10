5f1d73c3cf14e75c999e153535cb93bec4039165
unknown <3512037@alu.murciaeduca.es>
2023-02-10 21:44:10 +0100

    mvc 003

--- libs/setup.php
+++ libs/setup.php
@@ -5,8 +5,8 @@ $config->set('controllersFolder', 'controllers/');
 $config->set('modelsFolder', 'models/');
 $config->set('viewsFolder', 'views/');
  
-$config->set('dbhost', 'localhost');
-$config->set('dbname', 'almacen');
+$config->set('dbhost', 'containers-us-west-118.railway.app:7781/railway');
+$config->set('dbname', 'railway');
 $config->set('dbuser', 'root');
-$config->set('dbpass', '');
+$config->set('dbpass', 'ylfLHaF6RaaLTP9dGwjN');
 ?>
